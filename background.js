7c9765e371e8a82c1960d65035ec11de32a1b740
EHackC <lolisfun0987@gmail.com>
2025-07-11 00:04:10 +0000

    리다이렉트 페이지에서만 js 불러오도록 수정

--- background.js
+++ background.js
@@ -1,9 +1,6 @@
 var link = window.location.href;
-if (link.includes("playentry.org/redirect?external=")) {
-    var targetUrl = link.split("external=")[1];
-    console.log("이동중")
-    if (targetUrl) {
-        var decodedUrl = decodeURIComponent(targetUrl);
-        window.location.href = decodedUrl;
-    }
+var targetUrl = link.split("external=")[1];
+if (targetUrl) {
+    var decodedUrl = decodeURIComponent(targetUrl);
+    window.location.href = decodedUrl;
 }
