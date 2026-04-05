38a83133515c2375e65483b7904141d3b0be97e5
𒌧𒀱﷽ဪ꫞꧅m̷̬îͨ͋n͐ͯi̋̊r̕͢aͥ͌n̗g̚กิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิิ <minseong130502@gmail.com>
2026-04-05 11:37:24 +0900

    background.js 업데이트

--- background.js
+++ background.js
@@ -1,5 +1,6 @@
 var link = window.location.href;
 var targetUrl = link.split("external=")[1];
+
 if (targetUrl) {
     var decodedUrl = decodeURIComponent(targetUrl);
     window.location.href = decodedUrl;
